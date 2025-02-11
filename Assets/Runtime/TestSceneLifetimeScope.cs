77776acddc5561f4a2e0a038a7fbd2bd43da2ac3
ZerOx4C <zerox4c@gmail.com>
2025-02-11 16:41:25 +0900

    テスト用シーンを追加した

---
+++ Assets/Runtime/TestSceneLifetimeScope.cs
@@ -0,0 +1,12 @@
+using VContainer;
+using VContainer.Unity;
+
+namespace Runtime
+{
+    public class TestSceneLifetimeScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
