4ed72e574b9f8fdfb16cf2e23e128f1f3e500dc3
ZerOx4C <zerox4c@gmail.com>
2025-02-16 00:17:26 +0900

    Register周りを整理した

--- Assets/Runtime/TestSceneLifetimeScope.cs
+++ Assets/Runtime/TestSceneLifetimeScope.cs
@@ -20,24 +20,36 @@ namespace Runtime
         {
             base.Configure(builder);
 
+            // Behaviour
             builder.RegisterInstance(devilPrefab);
             builder.RegisterInstance(dicePrefab);
             builder.RegisterInstance(floorPrefab);
 
+            // Controller
+            builder.Register<PlayerDevilController>(Lifetime.Singleton);
+
+            // Entity
             builder.Register<Session>(Lifetime.Singleton);
 
-            builder.Register<FloorInitialization>(Lifetime.Singleton);
-            builder.Register<PlayerInitialization>(Lifetime.Singleton);
-            builder.Register<TransformConverter>(Lifetime.Singleton)
-                .As<ITransformConverter>()
-                .AsSelf();
-            builder.Register<DevilPresenter>(Lifetime.Singleton);
-            builder.Register<DicePresenter>(Lifetime.Singleton);
+            // Input
             builder.Register<PlayerInputSubject>(Lifetime.Singleton)
                 .As<IPlayerInputObservable>()
                 .AsSelf();
-            builder.Register<PlayerDevilController>(Lifetime.Singleton);
 
+            // Presenter
+            builder.Register<DevilPresenter>(Lifetime.Singleton);
+            builder.Register<DicePresenter>(Lifetime.Singleton);
+
+            // UseCase
+            builder.Register<FloorInitialization>(Lifetime.Singleton);
+            builder.Register<PlayerInitialization>(Lifetime.Singleton);
+
+            // Utility
+            builder.Register<TransformConverter>(Lifetime.Singleton)
+                .As<ITransformConverter>()
+                .AsSelf();
+
+            // EntryPoint
             builder.RegisterEntryPoint<TestEntryPoint>();
         }
     }
