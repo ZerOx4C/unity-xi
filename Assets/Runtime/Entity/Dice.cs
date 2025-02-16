543830638db30d1f18853956bab620e7a5321dea
ZerOx4C <zerox4c@gmail.com>
2025-02-16 21:56:13 +0900

    ダイスを転がすためのメンバを一旦追加した

--- Assets/Runtime/Entity/Dice.cs
+++ Assets/Runtime/Entity/Dice.cs
@@ -10,11 +10,13 @@ namespace Runtime.Entity
         private readonly ReactiveProperty<bool> _canClimb = new(false);
         private readonly ReactiveProperty<bool> _canOverride = new(false);
         private readonly ReactiveProperty<bool> _canPush = new(true);
+        private readonly ReactiveProperty<Vector2Int> _rollingDirection = new(Vector2Int.zero);
         private readonly ReactiveProperty<Vector2Int> _slidingDirection = new(Vector2Int.zero);
 
         public ReadOnlyReactiveProperty<bool> CanClimb => _canClimb;
         public ReadOnlyReactiveProperty<bool> CanOverride => _canOverride;
         public ReadOnlyReactiveProperty<bool> CanPush => _canPush;
+        public ReadOnlyReactiveProperty<Vector2Int> RollingDirection => _rollingDirection;
         public ReadOnlyReactiveProperty<Vector2Int> SlidingDirection => _slidingDirection;
 
         public void Dispose()
@@ -22,6 +24,7 @@ namespace Runtime.Entity
             _canClimb.Dispose();
             _canOverride.Dispose();
             _canPush.Dispose();
+            _rollingDirection.Dispose();
             _slidingDirection.Dispose();
         }
 
