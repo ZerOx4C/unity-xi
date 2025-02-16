a08703efad56b835020782e59c308e640b148b6e
ZerOx4C <zerox4c@gmail.com>
2025-02-16 15:26:50 +0900

    床の境界に壁を追加した＆変換ミスを修正した

--- Assets/Runtime/Behaviour/FloorBehaviour.cs
+++ Assets/Runtime/Behaviour/FloorBehaviour.cs
@@ -10,14 +10,13 @@ namespace Runtime.Behaviour
     public class FloorBehaviour : MonoBehaviour
     {
         public Transform cellPrefab;
+        public Transform wallPositiveX;
+        public Transform wallNegativeX;
+        public Transform wallPositiveZ;
+        public Transform wallNegativeZ;
+        public Transform floor;
 
-        private BoxCollider _boxCollider;
         private Transform[] _cells = { };
-
-        private void Awake()
-        {
-            _boxCollider = GetComponent<BoxCollider>();
-        }
 
         public void Clear()
         {
@@ -33,25 +32,43 @@ namespace Runtime.Behaviour
         {
             Clear();
 
+            var viewWidth = config.CellSize * config.FieldWidth;
+            var viewHeight = config.CellSize * config.FieldHeight;
+            SetupColliderSize(viewWidth, viewHeight);
+
             var cellCount = config.FieldWidth * config.FieldHeight;
             var offset = -0.5f * config.CellSize *
-                         new Vector3(config.FieldHeight - 1, 0, config.FieldWidth - 1);
+                         new Vector3(config.FieldWidth - 1, 0, config.FieldHeight - 1);
 
             var positions = new List<Vector3>(cellCount);
             var fieldBounds = new RectInt(0, 0, config.FieldWidth, config.FieldHeight);
             foreach (var p in fieldBounds.allPositionsWithin)
             {
-                positions.Add(config.CellSize * new Vector3(p.y, 0, p.x) + offset);
+                positions.Add(config.CellSize * new Vector3(p.x, 0, p.y) + offset);
             }
 
             var rotations = new Quaternion[cellCount];
             Array.Fill(rotations, Quaternion.identity);
 
-            _boxCollider.size = new Vector3(config.FieldWidth, 1, config.FieldHeight);
             _cells = await Instantiator.Create(cellPrefab)
                 .SetParent(transform)
                 .SetTransforms(positions, rotations)
                 .InstantiateAsync(cancellation).All;
+        }
+
+        private void SetupColliderSize(float xLength, float zLength)
+        {
+            wallPositiveX.position = new Vector3(xLength / 2, 0, 0);
+            wallNegativeX.position = new Vector3(-xLength / 2, 0, 0);
+            wallPositiveZ.position = new Vector3(0, 0, zLength / 2);
+            wallNegativeZ.position = new Vector3(0, 0, -zLength / 2);
+
+            wallPositiveX.localScale = new Vector3(1, 1, zLength);
+            wallNegativeX.localScale = new Vector3(1, 1, zLength);
+            wallPositiveZ.localScale = new Vector3(xLength, 1, 1);
+            wallNegativeZ.localScale = new Vector3(xLength, 1, 1);
+
+            floor.localScale = new Vector3(xLength, 1, zLength);
         }
 
         public class Config
