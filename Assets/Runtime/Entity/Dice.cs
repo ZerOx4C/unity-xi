3333fa442046ff9e34128a6909ad35596650e7f5
ZerOx4C <zerox4c@gmail.com>
2025-02-19 03:21:30 +0900

    ダイスの面がシミュレートされるようにした

--- Assets/Runtime/Entity/Dice.cs
+++ Assets/Runtime/Entity/Dice.cs
@@ -10,12 +10,14 @@ namespace Runtime.Entity
         private readonly ReactiveProperty<bool> _canClimb = new(false);
         private readonly ReactiveProperty<bool> _canOverride = new(false);
         private readonly ReactiveProperty<bool> _canPush = new(true);
+        private readonly ReactiveProperty<(int top, int front, int right)> _faceValues = new((1, 3, 5));
         private readonly ReactiveProperty<Vector2Int> _rollingDirection = new(Vector2Int.zero);
         private readonly ReactiveProperty<Vector2Int> _slidingDirection = new(Vector2Int.zero);
 
         public ReadOnlyReactiveProperty<bool> CanClimb => _canClimb;
         public ReadOnlyReactiveProperty<bool> CanOverride => _canOverride;
         public ReadOnlyReactiveProperty<bool> CanPush => _canPush;
+        public ReadOnlyReactiveProperty<(int top, int front, int right)> FaceValues => _faceValues;
         public ReadOnlyReactiveProperty<Vector2Int> RollingDirection => _rollingDirection;
         public ReadOnlyReactiveProperty<Vector2Int> SlidingDirection => _slidingDirection;
 
@@ -26,6 +28,32 @@ namespace Runtime.Entity
             _canPush.Dispose();
             _rollingDirection.Dispose();
             _slidingDirection.Dispose();
+        }
+
+        public void Roll(Vector2Int direction)
+        {
+            AssertUtility.IsValidDirection(direction);
+
+            var values = _faceValues.Value;
+
+            if (direction.x < 0)
+            {
+                values = (values.right, values.front, 7 - values.top);
+            }
+            else if (0 < direction.x)
+            {
+                values = (7 - values.right, values.front, values.top);
+            }
+            else if (direction.y < 0)
+            {
+                values = (7 - values.front, values.top, values.right);
+            }
+            else
+            {
+                values = (values.front, 7 - values.top, values.right);
+            }
+
+            _faceValues.Value = values;
         }
 
         public bool TryBeginPush(Vector2Int direction)
