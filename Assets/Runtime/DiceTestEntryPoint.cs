98e0e8c7ab7cb3e72f20679c6c92ed78535d1a99
ZerOx4C <zerox4c@gmail.com>
2025-02-28 01:54:16 +0900

    ダイス姿勢のランダム化が正しく動いているかの検証を追加した

--- Assets/Runtime/DiceTestEntryPoint.cs
+++ Assets/Runtime/DiceTestEntryPoint.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using R3;
 using Runtime.Behaviour;
 using Runtime.Entity;
@@ -7,6 +9,7 @@ using Runtime.Utility;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
+using Random = UnityEngine.Random;
 
 namespace Runtime
 {
@@ -36,6 +39,9 @@ namespace Runtime
 
         public void Start()
         {
+            TestRandomize("Randomize1", 10000, Randomize1);
+            TestRandomize("Randomize2", 10000, Randomize2);
+
             var diceBehaviour = Instantiator.Create(_diceBehaviourPrefab).Instantiate().First;
 
             _playerInput.Move.OnBegan
@@ -58,6 +64,57 @@ namespace Runtime
         public void Tick()
         {
             _playerInput.Tick();
+        }
+
+        private static void TestRandomize(string title, int count, Action<Dice> randomizer)
+        {
+            var result = new Dictionary<(int top, int front), int>();
+            for (var _ = 0; _ < count; _++)
+            {
+                var dice = new Dice();
+                randomizer(dice);
+
+                var key = (dice.FaceValues.CurrentValue.top, dice.FaceValues.CurrentValue.front);
+                result[key] = result.GetValueOrDefault(key) + 1;
+            }
+
+            var odds = string.Join(", ", result
+                .GroupBy(p => p.Key.top)
+                .OrderBy(p => p.Key)
+                .Select(g => $"{g.Key}: {100f * g.Sum(p => p.Value) / count:f1}%"));
+
+            Debug.Log($"[{title}] {odds}");
+        }
+
+        private static void Randomize1(Dice dice)
+        {
+            // TODO: パターン数的に偏ってるハズなので要改善
+            var x = Random.Range(0, 4);
+            while (0 < x--)
+            {
+                dice.Roll(Vector2Int.right);
+            }
+
+            var y = Random.Range(0, 4);
+            while (0 < y--)
+            {
+                dice.Roll(Vector2Int.up);
+            }
+        }
+
+        private static void Randomize2(Dice dice)
+        {
+            var count = Random.Range(0, 6);
+            for (var i = 0; i < count; i++)
+            {
+                dice.Roll(i % 2 == 0 ? Vector2Int.right : Vector2Int.up);
+            }
+
+            count = Random.Range(0, 4);
+            while (0 < count--)
+            {
+                dice.Roll(Vector2Int.up);
+            }
         }
     }
 }
