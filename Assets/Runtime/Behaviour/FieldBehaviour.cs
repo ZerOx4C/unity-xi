c39e8676b89d2e82a0dc600992d6b4c8b56ca343
ZerOx4C <zerox4c@gmail.com>
2025-03-02 04:24:59 +0900

    フィールドのパネルを再利用するようにした

--- Assets/Runtime/Behaviour/FieldBehaviour.cs
+++ Assets/Runtime/Behaviour/FieldBehaviour.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
@@ -16,9 +15,9 @@ namespace Runtime.Behaviour
         public Transform wallPositiveZ;
         public Transform wallNegativeZ;
         public Transform floor;
+        private readonly List<Transform> _cells = new();
 
         private CancellationDisposable _cancellationDisposable;
-        private Transform[] _cells = { };
         private ReactiveProperty<bool> _isReady;
 
         public ReadOnlyReactiveProperty<bool> IsReady => _isReady;
@@ -33,10 +32,8 @@ namespace Runtime.Behaviour
         {
             foreach (var cell in _cells)
             {
-                Destroy(cell.gameObject);
+                cell.gameObject.SetActive(false);
             }
-
-            _cells = Array.Empty<Transform>();
         }
 
         public void BeginSetup(float cellSize, int width, int height)
@@ -55,22 +52,18 @@ namespace Runtime.Behaviour
         private async UniTask SetupFloorCells(float cellSize, int width, int height, CancellationToken cancellation)
         {
             var cellCount = width * height;
+            await EnsureCellsAsync(cellCount, cancellation);
+
             var offset = -0.5f * cellSize * new Vector3(width - 1, 0, height - 1);
 
-            var positions = new List<Vector3>(cellCount);
+            var i = 0;
             var fieldBounds = new RectInt(0, 0, width, height);
             foreach (var p in fieldBounds.allPositionsWithin)
             {
-                positions.Add(cellSize * new Vector3(p.x, 0, p.y) + offset);
+                var cell = _cells[i++];
+                cell.transform.position = cellSize * new Vector3(p.x, 0, p.y) + offset;
+                cell.gameObject.SetActive(true);
             }
-
-            var rotations = new Quaternion[cellCount];
-            Array.Fill(rotations, Quaternion.identity);
-
-            _cells = await Instantiator.Create(cellPrefab)
-                .SetParent(transform)
-                .SetTransforms(positions, rotations)
-                .InstantiateAsync(cancellation).All;
         }
 
         private void SetupColliders(float width, float height)
@@ -86,6 +79,21 @@ namespace Runtime.Behaviour
             wallNegativeZ.localScale = new Vector3(width, 1, 1);
 
             floor.localScale = new Vector3(width, 1, height);
+        }
+
+        private async UniTask EnsureCellsAsync(int count, CancellationToken cancellation)
+        {
+            if (count <= _cells.Count)
+            {
+                return;
+            }
+
+            var cells = await Instantiator.Create(cellPrefab)
+                .SetParent(transform)
+                .SetTransforms(count - _cells.Count, Vector3.zero, Quaternion.identity)
+                .InstantiateAsync(cancellation).All;
+
+            _cells.AddRange(cells);
         }
 
         private CancellationToken ObtainCancellation()
