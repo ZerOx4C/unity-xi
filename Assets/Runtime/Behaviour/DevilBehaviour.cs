5a9a1073141f4d5ebcc6170f56fef73ceda7f897
ZerOx4C <zerox4c@gmail.com>
2025-03-09 01:39:40 +0900

    ユニティちゃんがプレイヤーとして歩くようにした

--- Assets/Runtime/Behaviour/DevilBehaviour.cs
+++ Assets/Runtime/Behaviour/DevilBehaviour.cs
@@ -4,6 +4,10 @@ namespace Runtime.Behaviour
 {
     public class DevilBehaviour : MonoBehaviour
     {
+        private static readonly int SpeedParameter = Animator.StringToHash("Speed");
+
+        public Animator animator;
+
         private Rigidbody _rigidbody;
 
         private void Awake()
@@ -14,6 +18,8 @@ namespace Runtime.Behaviour
         public void SetVelocity(Vector3 velocity)
         {
             _rigidbody.linearVelocity = velocity;
+
+            animator.SetFloat(SpeedParameter, velocity.magnitude);
         }
 
         public void SetDirection(Vector3 direction)
---
+++ Assets/Runtime/Component/DummyReceiver.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace Runtime.Component
+{
+    public class DummyReceiver : MonoBehaviour
+    {
+        private void OnCallChangeFace(string str)
+        {
+        }
+    }
+}
